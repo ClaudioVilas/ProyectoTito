677e3ae25b93e30b22a79dcc8b6d0c9a597ea5b5
Claudio Vilas <claudiogvilas@gmail.com>
2023-01-20 11:53:59 -0300

    finally

--- JavaScript/menuhamburgesa.js
+++ JavaScript/menuhamburgesa.js
@@ -1,12 +1,12 @@
-const iconoMenu = document.querySelector ("#icon-menu")
+const iconoMenu = document.querySelector ("#iconmenuhamburgesa")
 
 const ocultar = document.querySelector ("#businesshamburgesa")
 
 const ocultar2 = document.querySelector ("#workerhamburgesa")
 
-// const ocultar3 = document.querySelector ("#downloadhamburgesa")
+const ocultar3 = document.querySelector ("#downloadhamburgesa")
 
-    iconoMenu.addEventListener ("click", () => {
+    iconoMenu.addEventListener ("click", (e) => {
         menu.classList.toggle ("mostrar")
             document.body.classList.toggle ("opacity")
     })
@@ -23,10 +23,10 @@ const ocultar2 = document.querySelector ("#workerhamburgesa")
             document.body.classList.toggle ("opacity")
     })
 
-    // ocultar3.addEventListener ("click", () => {
-    //     document.body.classList.toggle ("opacity")
-    //     menu.classList.toggle ("mostrar")
-    // })
+    ocultar3.addEventListener ("click", () => {
+        document.body.classList.toggle ("opacity")
+        menu.classList.toggle ("mostrar")
+    })
 
 
 
