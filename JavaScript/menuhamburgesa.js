bad17e2b4232a22510d639a8bbdaca952095092d
Claudio Vilas <claudiogvilas@gmail.com>
2023-01-20 11:03:40 -0300

    360

--- JavaScript/menuhamburgesa.js
+++ JavaScript/menuhamburgesa.js
@@ -4,6 +4,8 @@ const ocultar = document.querySelector ("#businesshamburgesa")
 
 const ocultar2 = document.querySelector ("#workerhamburgesa")
 
+// const ocultar3 = document.querySelector ("#downloadhamburgesa")
+
     iconoMenu.addEventListener ("click", () => {
         menu.classList.toggle ("mostrar")
             document.body.classList.toggle ("opacity")
@@ -20,6 +22,11 @@ const ocultar2 = document.querySelector ("#workerhamburgesa")
         menu.classList.toggle ("mostrar")
             document.body.classList.toggle ("opacity")
     })
+
+    // ocultar3.addEventListener ("click", () => {
+    //     document.body.classList.toggle ("opacity")
+    //     menu.classList.toggle ("mostrar")
+    // })
 
 
 
