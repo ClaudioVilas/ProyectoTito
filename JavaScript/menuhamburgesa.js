a7a64ac06a6125f75cbbf7035222049a61b2f187
Claudio Vilas <claudiogvilas@gmail.com>
2023-01-20 07:59:58 -0300

    menu hamburgesa

---
+++ JavaScript/menuhamburgesa.js
@@ -0,0 +1,17 @@
+const iconoMenu = document.querySelector ("#icon-menu")
+
+const ocultar = document.querySelector ("#businesshamburgesa")
+
+    iconoMenu.addEventListener ("click", () => {
+        menu.classList.toggle ("active")
+            document.body.classList.toggle ("opacity")
+    })
+
+
+    ocultar.addEventListener ("click", () => {
+        menu.classList.toggle ("active")
+            document.body.classList.toggle ("opacity")
+    })
+
+
+
